8e03b7aeb2df0befb13265e021e61c9ac0a3d777
Kevin Perdlich <kevin.perdlich@gmx.de>
2018-10-16 14:12:00 +0200

    added Praktikum 1

---
+++ GIP-2018-2019/Praktikum 1/HalloWelt/HalloWelt.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	/*cout << "Hallo Welt!" << endl;
+	system("PAUSE");*/
+	{	
+		cout << "Hallo Welt!" << endl; 
+	}
+	{	
+		system("PAUSE"); }
+	{
+		return 0; 
+	}
+}
