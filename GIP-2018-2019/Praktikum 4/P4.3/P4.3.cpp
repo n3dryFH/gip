a6e106b75a713004d363e1dba6aa4c05c585d45f
Kevin Perdlich <kevin.perdlich@gmx.de>
2018-11-13 12:53:09 +0100

    fixed P4.3 for cycle > 60

--- GIP-2018-2019/Praktikum 4/P4.3/P4.3.cpp
+++ GIP-2018-2019/Praktikum 4/P4.3/P4.3.cpp
@@ -4,23 +4,19 @@ using namespace std;
 int main()
 {
 	int startHour = 0, startMinutes = 0, cycleMinutes = 0;
-	const int dayInSecs = 24 * 60 * 60;
-	do
-	{
+	do {
 		cout << "Bitte geben Sie die Stunden der Startuhrzeit ein: ";
 		cin >> startHour;
 	} while (startHour < 0 || startHour >= 24);
 
-	do
-	{
+	do {
 		cout << "Bitte geben Sie die Minuten der Startuhrzeit ein: ";
 		cin >> startMinutes;
 	} while (startMinutes < 0 || startMinutes > 59);
 
 	cout << "Der erste Bus faehrt also um " << startHour << ":" << startMinutes << " Uhr." << endl;
 
-	do
-	{
+	do {
 		cout << "Bitte geben Sie die Taktzeit in Minuten ein: ";
 		cin >> cycleMinutes;
 
@@ -33,16 +29,19 @@ int main()
 	bool isLastDriveForToday = false;
 	do
 	{
-		for (unsigned int currentRowMinutes = ((currentStartTimeInSec / 60) % 60); currentRowMinutes < 60;
-			currentRowMinutes += cycleMinutes, currentStartTimeInSec += cycleInSec)
+		for (unsigned int currentMinutes = ((currentStartTimeInSec / 60) % 60); currentMinutes < 60; currentMinutes += cycleMinutes)
 		{
-			cout << currentStartTimeInSec / 60 / 60 << ":" << currentRowMinutes << " ";
+			cout << currentStartTimeInSec / 60 / 60 << ":" << (currentStartTimeInSec / 60) % 60 << " ";					
+			if (currentStartTimeInSec + cycleInSec >= 24 * 60 * 60)
+			{
+				isLastDriveForToday = true;
+			}
+			else 
+			{
+				currentStartTimeInSec += cycleInSec;
+			}			
 		}
-		cout << endl;
-		if (currentStartTimeInSec + cycleInSec >= dayInSecs)
-		{
-			isLastDriveForToday = true;
-		}
+		cout << endl;		
 	} while (!isLastDriveForToday);
 
 	system("PAUSE");
